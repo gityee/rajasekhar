f21d9b5e6924af3c8474fd53ca0aaed379a33d24
s8 <s8@s8-PC>
2018-04-18 23:52:15 -0700

    v3

--- src/Gitpackage/Git_class.java
+++ src/Gitpackage/Git_class.java
@@ -3,8 +3,7 @@ package Gitpackage;
 public class Git_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("this is test message");
 	}
 
 }
